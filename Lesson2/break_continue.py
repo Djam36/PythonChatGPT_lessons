c3b74efb04cfa610f43a17d3a87a3e971a8c3185
Evgeniy Tsigelnikov <etsigelnikov@fil-it.ru>
2023-07-12 02:22:06 +0300

    Add Lesson2

---
+++ Lesson2/break_continue.py
@@ -0,0 +1,12 @@
+for i in range(1, 6):
+    if i == 3:
+        continue
+    print(i)
+
+
+fruits = ["apple", "banana", "cherry"]
+
+for fruit in fruits:
+    if fruit == "banana":
+        break
+    print(fruit)
---
+++ Lesson2/loops.py
@@ -0,0 +1,11 @@
+x = 0
+
+while x < 5:
+    print(x)
+    x += 1
+
+
+fruits = ["apple", "banana", "cherry"]
+
+for fruit in fruits:
+    print(fruit)
